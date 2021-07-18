053040135a3366ec0e4066c425cf8e8c0ee5e4d6
Lucas de Oliveira <lucas.faiskaa@gmail.com>
2021-07-17 21:00:16 -0300

    feat: adds alert dialog when user not found

--- app/src/main/java/com/lfaiska/codewars/app/presentation/scenes/users/UsersFragment.kt
+++ app/src/main/java/com/lfaiska/codewars/app/presentation/scenes/users/UsersFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.lfaiska.codewars.app.MainApplication
@@ -15,7 +17,7 @@ class UsersFragment : Fragment() {
     private lateinit var binding: FragmentUsersBinding
 
     @Inject
-    lateinit var usersViewModel: UsersViewModel
+    lateinit var viewModel: UsersViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (activity?.applicationContext as MainApplication).appComponent.inject(this)
@@ -29,6 +31,30 @@ class UsersFragment : Fragment() {
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_users, container, false)
+        setupSearchView()
+        setupObservers()
         return binding.root
+    }
+
+    private fun setupSearchView() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                viewModel.searchUser(query)
+                return false
+            }
+        })
+    }
+
+    private fun setupObservers() {
+        viewModel.hasUserNotFoundError.observe(viewLifecycleOwner, { hasFailure ->
+            if (hasFailure) {
+                AlertDialog.Builder(requireContext())
+                    .setMessage(getString(R.string.user_not_found_error_message)).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/lfaiska/codewars/app/presentation/scenes/users/UsersViewModel.kt
+++ app/src/main/java/com/lfaiska/codewars/app/presentation/scenes/users/UsersViewModel.kt
@@ -1,9 +1,29 @@
 package com.lfaiska.codewars.app.presentation.scenes.users
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lfaiska.codewars.app.presentation.component.SingleLiveEvent
+import com.lfaiska.codewars.domain.error.UserNotFoundException
 import com.lfaiska.codewars.domain.usecase.GetUserByUsername
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class UsersViewModel @Inject constructor(val getUserByUsername: GetUserByUsername) : ViewModel() {
+class UsersViewModel @Inject constructor(
+    private val getUserByUsername: GetUserByUsername
+) : ViewModel() {
+
+    private val _hasUserNotFoundError = SingleLiveEvent<Boolean>()
+    val hasUserNotFoundError: LiveData<Boolean> = _hasUserNotFoundError
+
+    fun searchUser(userName: String) {
+        viewModelScope.launch {
+            try {
+                getUserByUsername.execute(userName)
+            } catch (exception: Exception) {
+                _hasUserNotFoundError.postValue(true)
+            }
+        }
+    }
 
 }
